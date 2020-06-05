3bea9229328f62617b89d80e566d6a0852605489
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-06-05 09:54:39 +0100

    set express env. close #2.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const url = 'mongodb+srv://user:1234@mongoformation-xc16w.mongodb.net/glsiC?retryWrites=true&w=majority';
 require('./db/connection')(url);
+
+const express = require('express');
+const morgan = require('morgan');
+const port = process.env.PORT || 3000;
+const app_debug = require('debug')('app:debug');
+const app = express();
+
+app.use(express.json());
+if(app.get('env') === 'development')
+    app.use(morgan('dev'));
+
+
+app.listen(port, () => app_debug(`Server running on ${port}...`));
