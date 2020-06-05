47bbb4f3e1d9fe338782841370a8a5b3a2f8daa4
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-06-05 09:39:29 +0100

    create Mongo connection. close #1.

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const mongo_debug = require('debug')('app:mongo');
+
+module.exports = async function connect(url){
+    try{
+        await mongoose.connect(url,{ useNewUrlParser : true, useUnifiedTopology:true});
+        mongo_debug('Mongo is Up.');
+    }catch(err){
+        mongo_debug(`Mongo is down, raison : ${err.message}`);
+    }
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+const url = 'mongodb+srv://user:1234@mongoformation-xc16w.mongodb.net/glsiC?retryWrites=true&w=majority';
+require('./db/connection')(url);
